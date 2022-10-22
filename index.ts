7d8e8d91d40b10123d89c8ff83faf1193fb9a4f8
Nenne Nwodo <nennenwodo@Nennes-MacBook-Pro.local>
2022-10-22 07:19:55 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,48 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azureNative from "@pulumi/azure-native";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+
+const stack = pulumi.getStack();
+const resourceGroupName = `cityjs-app-resourcegroup-${stack}`;
+const appServicePlanName = `cityjs-app-plan-${stack}`;
+const storageAccountName = `cityjsstore${stack}`;
+const webAppName = `cityjs-app-${stack}`;
+
+// Create an Azure Resource Group
+const resourceGroup = new resources.ResourceGroup(resourceGroupName, {
+    resourceGroupName,
+    location: "EastUS"
+});
+
+// Create an Azure resource (Storage Account)
+const storageAccount = new storage.StorageAccount(storageAccountName, {
+    accountName: storageAccountName,
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    sku: {
+        name: storage.SkuName.Standard_LRS,
+    },
+    kind: storage.Kind.StorageV2,
+});
+
+const appServicePlan = new azureNative.web.AppServicePlan(appServicePlanName,
+{
+    name: appServicePlanName,
+    kind: "app",
+    location: resourceGroup.location,
+    resourceGroupName: resourceGroup.name,
+    sku: {
+        capacity: 1,
+        name: "P2V2",
+        size: "P2V2",
+        tier: "PremiumV2"
+    }
+});
+
+const webApp = new azureNative.web.WebApp(webAppName, {
+    name: webAppName,
+    resourceGroupName: resourceGroup.name,
+    serverFarmId: appServicePlan.id,
+    location: resourceGroup.location
+});
